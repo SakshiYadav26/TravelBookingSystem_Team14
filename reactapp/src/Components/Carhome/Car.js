8406a3e163c847b4452e711f8921844f4dffd326
Rajalakshmi G <66936135+RajiG08@users.noreply.github.com>
2023-06-14 22:11:11 +0530

    carhome modified

--- reactapp/src/Components/Carhome/Car.js
+++ reactapp/src/Components/Carhome/Car.js
@@ -1,30 +1,30 @@
-import React from 'react'
-import Styles from "./CarHome.module.css";
+import React from 'react';
+import './CarHome.css';
 
-const Car = ({image,Heading,Description,setImageState}) => {
-  const setWindowLocation = ()=>{
-    window.localStorage.setItem('currentImage',image);
-    window.localStorage.setItem("Heading",Heading);
-  }
+const Car = ({ image, Heading, Description, setImageState }) => {
+  const setWindowLocation = () => {
+    window.localStorage.setItem('currentImage', image);
+    window.localStorage.setItem('Heading', Heading);
+  };
+
   return (
-    <div className='card col-lg-5 my-3 py-3 mx-3'>
-        <div className='row'>
-            <div className='col-lg-5'>
-                <img src = {image} alt="Car"/>
-            </div>
-            <div className='col-lg-7'>
-                <p className={`${Styles.carName}`}>{Heading}</p>
-                <p className='text text-justify'>{Description}</p>
-                <div className={`${Styles.viewDetailsBtnDiv}`}>
-                <button className={`${Styles.viewDetailsBtn}`}
-                onClick={setWindowLocation}>
-                  <a href={`/viewDetails/image/Heading/`} className="text text-white">View Details</a>
-                </button>
-                </div>
-            </div>
+    <div className="card col-lg-5 my-3 py-3 mx-3">
+      <div className="row">
+        <div className="col-lg-5">
+          <img src={image} alt="Car" className="img-fluid" />
         </div>
+        <div className="col-lg-7">
+          <p className="car-name">{Heading}</p>
+          <p className="text-justify">{Description}</p>
+          <div className="text-right">
+            <button className="btn btn-primary view-details-btn" onClick={setWindowLocation}>
+              <a href={`/viewDetails/${image}/${Heading}/`} className="text-white">View Details</a>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Car
+export default Car;
--- reactapp/src/Components/Carhome/CarHome.js
+++ reactapp/src/Components/Carhome/CarHome.js
@@ -1,174 +1,108 @@
-import React,{useState} from "react";
-import Styles from "./CarHome.module.css";
+import React, { useState } from "react";
 import Car from "./Car";
+import './CarHome.css';
 import FirstImage from "../Images/FirstCarImages.jpg";
 import SecondImage from "../Images/SecondCarImages.jpg";
 import ThirdImage from "../Images/ThirdCarImages.jpg";
 import FourthImage from "../Images/FourthCarImages.jpg";
-import { DatePicker,TimePicker, Select } from "antd";;
+import { DatePicker, TimePicker, Select } from "antd";
 
-//const { RangePicker } = DatePicker;
+const { Option } = Select;
+
 const CarHome = () => {
-const[imageState,setImageState] = useState("");
+  const [imageState, setImageState] = useState("");
+
+  const setWindowLocation = (image, heading) => {
+    window.localStorage.setItem("currentImage", image);
+    window.localStorage.setItem("Heading", heading);
+  };
 
   return (
     <div className="container mt-5">
-      <div className={`row ${Styles.firstRow}`}>
-      <p className={Styles.textCenter}>Grab your car!!! And Enjoy your Vacations!!! </p>
+      <div className="row card py-3">
         <div className="col-lg-12">
+          <p className="text-center">Grab your car!!! And Enjoy your Vacations!!!</p>
           <div className="row pb-3">
             <div className="col-lg-4 card">
               <p className="mt-4">Pickup Location:</p>
-              <Select
-                defaultValue="Hyderabad"
-                style={{
-                  width: 200,
-                }}
-                onChange=""
-                options={[
-                  {
-                    value: "Hyderabad",
-                    label: "Hyderabad",
-                  },
-                  {
-                    value: "Vikshakapatnam",
-                    label: "Vikshakapatna",
-                  },
-                  {
-                    value: "Bangloore",
-                    label: "Bangloore",
-                  },
-                  {
-                    value: "Mumbai",
-                    label: "Mumbai",
-                  },
-                  {
-                    value: "Chennai",
-                    label: "Chennai",
-                  },
-                  {
-                    value: "Delhi",
-                    label: "Delhi",
-                  },
-                  {
-                    value: "Pune",
-                    label: "Pune",
-                  },
-                  {
-                    value: "Kolkatta",
-                    label: "Kolkatta",
-                  },
-                ]}
-              />
+              <Select defaultValue="Hyderabad" style={{ width: 200 }}>
+                <Option value="Hyderabad">Hyderabad</Option>
+                <Option value="Vikshakapatnam">Vikshakapatnam</Option>
+                <Option value="Bangloore">Bangloore</Option>
+                <Option value="Mumbai">Mumbai</Option>
+                <Option value="Chennai">Chennai</Option>
+                <Option value="Delhi">Delhi</Option>
+                <Option value="Pune">Pune</Option>
+                <Option value="Kolkatta">Kolkatta</Option>
+              </Select>
             </div>
             <div className="col-lg-2 card py-2">
               <p>Drop Location:</p>
-              <Select
-                defaultValue="Hyderabad"
-                style={{
-                  width: 165,
-                }}
-                onChange=""
-                options={[
-                  {
-                    value: "Hyderabad",
-                    label: "Hyderabad",
-                  },
-                  {
-                    value: "Vikshakapatnam",
-                    label: "Vikshakapatna",
-                  },
-                  {
-                    value: "Bangloore",
-                    label: "Bangloore",
-                  },
-                  {
-                    value: "Mumbai",
-                    label: "Mumbai",
-                  },
-                  {
-                    value: "Chennai",
-                    label: "Chennai",
-                  },
-                  {
-                    value: "Delhi",
-                    label: "Delhi",
-                  },
-                  {
-                    value: "Pune",
-                    label: "Pune",
-                  },
-                  {
-                    value: "Kolkatta",
-                    label: "Kolkatta",
-                  },
-                ]}
-              />
-              
-              
+              <Select defaultValue="Hyderabad" style={{ width: 165 }}>
+                <Option value="Hyderabad">Hyderabad</Option>
+                <Option value="Vikshakapatnam">Vikshakapatnam</Option>
+                <Option value="Bangloore">Bangloore</Option>
+                <Option value="Mumbai">Mumbai</Option>
+                <Option value="Chennai">Chennai</Option>
+                <Option value="Delhi">Delhi</Option>
+                <Option value="Pune">Pune</Option>
+                <Option value="Kolkatta">Kolkatta</Option>
+              </Select>
             </div>
             <div className="col-lg-2 card py-2">
               <p>pickup Date:</p>
               <DatePicker />
-              
-              
             </div>
             <div className="col-lg-2 card">
               <p>Drop Date:</p>
-              <DatePicker></DatePicker>
-              
+              <DatePicker />
             </div>
             <div className="col-lg-2 card">
               <p>Pickup Time:</p>
-             <TimePicker />
+              <TimePicker />
             </div>
           </div>
         </div>
       </div>
-      <div className={`row ${Styles.secondRow}`}>
-        <div>
-          <button className={Styles.searchBtn}>SEARCH</button>
-        </div>
-      </div>
 
-      <div className={`row ${Styles.thirdRow} py-3`}>
-        <div className="col-lg-12 card">
-          <div className="d-flex flex-row justify-content-around ">
+      <div className="row card py-3">
+        <div className="col-lg-12">
+          <div className="d-flex flex-row justify-content-around">
             <h3>Offers</h3>
-            
             <p>Cabs</p>
             <p>Bank Offers</p>
           </div>
         </div>
+      </div>
 
+      <div className="row">
         <Car
           image={FirstImage}
-          setImageState = {setImageState}
-          
+          setImageState={setImageState}
           Heading="Sedan Cars"
-
-          Description="Sedan Cars are best for family useage!!! In sedan cars Five members car travel easily and best boot Space.."
+          Description="Sedan Cars are best for family usage!!! In sedan cars, five members can travel easily with ample boot space."
+          setWindowLocation={setWindowLocation}
         />
         <Car
           image={SecondImage}
-          setImageState = {setImageState}
-
+          setImageState={setImageState}
           Heading="Miniature Cars"
-          Description="Mini cars are best for Small Family useage!!! In mini cars we can travel four Members confortably and driving cast is less.."
+          Description="Mini cars are best for small family usage!!! In mini cars, four members can travel comfortably with lower fuel costs."
+          setWindowLocation={setWindowLocation}
         />
         <Car
           image={ThirdImage}
-          setImageState = {setImageState}
-
+          setImageState={setImageState}
           Heading="SUV Cars"
-          Description="Suv cars are also best for Family useage!!! In Suv cars five Members car Travel easily!But there is no boot space.."
+          Description="SUV cars are also great for family usage!!! In SUV cars, five members can travel easily, although there is limited boot space."
+          setWindowLocation={setWindowLocation}
         />
         <Car
           image={FourthImage}
-          setImageState = {setImageState}
-
+          setImageState={setImageState}
           Heading="MPV Cars"
-          Description="Mpv cars are best usage for combine family travel!!! In Mpv cars more than six members can travel,But Running cost is high.."
+          Description="MPV cars are perfect for large family travel!!! In MPV cars, more than six members can travel comfortably, but running costs may be higher."
+          setWindowLocation={setWindowLocation}
         />
       </div>
     </div>
