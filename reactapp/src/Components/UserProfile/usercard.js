6e9c96c03467cd995bce88b67b1a7e464c0a6825
Rajalakshmi G <66936135+RajiG08@users.noreply.github.com>
2023-06-23 18:17:08 +0530

    userprofile comp cntd to backnd & axios installed

--- reactapp/src/Components/UserProfile/usercard.js
+++ reactapp/src/Components/UserProfile/usercard.js
@@ -1,124 +1,140 @@
-import React from 'react';
-import './style.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './style.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useLocation } from 'react-router-dom';
 
 const UserProfile = () => {
   const location = useLocation();
-    const queryParams = new URLSearchParams(location.search);
-  
-    const fullName = queryParams.get('fullName');
-    const email = queryParams.get('email');
-    const phone = queryParams.get('phone');
-    const age = queryParams.get('age');
-    const gender = queryParams.get('gender');
-    const address = queryParams.get('address');
-    const city = queryParams.get('city');
-    const state = queryParams.get('state');
-    const pincode = queryParams.get('pincode');
+  const queryParams = new URLSearchParams(location.search);
+
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const email = queryParams.get('email')
+        // const response = await axios.get('/api/v1/user?email=${email}'); 
+        const response = await axios.get(`/api/v1/user?email=${email}`);
+// Replace with the appropriate API endpoint to fetch user data
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-   
     <div className="card mb-3">
-        <button type="button" class="btn btn-primary" style={{fontSize:'30px'}}>
-  User Profile <span class="badge badge-light"></span>
-  
-</button>
+      <button type="button" className="btn btn-primary" style={{ fontSize: '30px' }}>
+        User Profile <span className="badge badge-light"></span>
+      </button>
       <div className="card-body">
+        {userData ? (
+          <>
+           <div className="card-body">
       
-        <div className="row" style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0"  style={{fontsize:'20px'}}>Full Name</h6>
-          </div>
-          <div className="col-sm-9 text-secondary">
-            {fullName}
-          </div>
+      <div className="row" style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0"  style={{fontSize:'20px'}}>Full Name</h6>
         </div>
-        <hr />
-        <div className="row" style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0"  style={{fontsize:'20px'}}>Email</h6>
-          </div>
-          <div className="col-sm-9 text-secondary" >
-            {email}
-          </div>
+
+        {/* variable name has to be verified */}
+        <div className="col-sm-9 text-secondary">   
+          {userData.fullname}           
         </div>
-        <hr />
-        <div className="row"  style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0" >Phone</h6>
-          </div>
-          <div className="col-sm-9 text-secondary" >
-            {phone}
-          </div>
+      </div>
+      <hr />
+      <div className="row" style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0"  style={{fontSize:'20px'}}>Email</h6>
         </div>
-        <hr />
-        <div className="row"  style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0">Age </h6>
-          </div>
-          <div className="col-sm-9 text-secondary" >
-            {age}
-          </div>
+        <div className="col-sm-9 text-secondary" >
+          {userData.email}
         </div>
-        <hr />
-        <div className="row"  style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0">Gender</h6>
-          </div>
-          <div className="col-sm-9 text-secondary">
-            {gender}
-          </div>
+      </div>
+      <hr />
+      <div className="row"  style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0" >Phone</h6>
         </div>
-        <hr />
-        
-        <div className="row"  style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0">Address</h6>
-          </div>
-          <div className="col-sm-9 text-secondary" >
-            {address}
-          </div>
+        <div className="col-sm-9 text-secondary" >
+          {userData.phone}
         </div>
-        <hr />
-        <div className="row"  style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0">City</h6>
-          </div>
-          <div className="col-sm-9 text-secondary">
-            {city}
-          </div>
+      </div>
+      <hr />
+      <div className="row"  style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0">Age </h6>
         </div>
-        <hr />
-        <div className="row"  style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0">State</h6>
-          </div>
-          <div className="col-sm-9 text-secondary" >
-            {state}
-          </div>
+        <div className="col-sm-9 text-secondary" >
+          {userData.age}
         </div>
-        <hr />
-        <div className="row"  style={{fontsize:'20px'}}>
-          <div className="col-sm-3">
-            <h6 className="mb-0" >Pincode</h6>
-          </div>
-          <div className="col-sm-9 text-secondary"  >
-            {pincode}
-          </div>
+      </div>
+      <hr />
+      <div className="row"  style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0">Gender</h6>
         </div>
-        <hr />
-        
-        <div className="row"  style={{fontsize:'20px'}}>
-          <div className="col-sm-12">
-            <a className="btn btn-info" target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
-          </div>
+        <div className="col-sm-9 text-secondary">
+          {userData.gender}
+        </div>
+      </div>
+      <hr />
+      
+      <div className="row"  style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0">Address</h6>
+        </div>
+        <div className="col-sm-9 text-secondary" >
+          {userData.address}
+        </div>
+      </div>
+      <hr />
+      <div className="row"  style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0">City</h6>
+        </div>
+        <div className="col-sm-9 text-secondary">
+          {userData.city}
+        </div>
+      </div>
+      <hr />
+      <div className="row"  style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0">State</h6>
+        </div>
+        <div className="col-sm-9 text-secondary" >
+          {userData.state}
+        </div>
+      </div>
+      <hr />
+      <div className="row"  style={{fontSize:'20px'}}>
+        <div className="col-sm-3">
+          <h6 className="mb-0" >Pincode</h6>
+        </div>
+        <div className="col-sm-9 text-secondary"  >
+          {userData.pincode}
+        </div>
+      </div>
+      <hr />
+      
+      <div className="row"  style={{fontSize:'20px'}}>
+        <div className="col-sm-12">
+          <a className="btn btn-info" target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
         </div>
       </div>
     </div>
-    
-
-    
-        
+            
+          </>
+        ) : (
+          <div>Loading user data...</div>
+        )}
+        {/* Add the remaining HTML code */}
+      </div>
+    </div>
   );
 };
 
